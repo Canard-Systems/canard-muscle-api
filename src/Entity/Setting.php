1c923aaf41f7db8b6893ef61e59edebcf89aadb8
CallMeTrinity <anteur.ap@gmail.com>
2025-01-27 18:16:24 +0100

    feat : admin settings

---
+++ src/Entity/Setting.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Metadata\ApiResource;
+use App\Repository\SettingRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: SettingRepository::class)]
+#[ApiResource(
+    security: ("is_granted('ROLE_SUPER_ADMIN')"),
+)]
+class Setting
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $settingKey = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $value = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getSettingKey(): ?string
+    {
+        return $this->settingKey;
+    }
+
+    public function setSettingKey(string $settingKey): static
+    {
+        $this->settingKey = $settingKey;
+
+        return $this;
+    }
+
+    public function getValue(): ?string
+    {
+        return $this->value;
+    }
+
+    public function setValue(string $value): static
+    {
+        $this->value = $value;
+
+        return $this;
+    }
+}
---
+++ src/Entity/UserSetting.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\UserSettingRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: UserSettingRepository::class)]
+class UserSetting
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\ManyToOne(inversedBy: 'userSettings')]
+    private ?User $user = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $settingKey = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $value = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getUser(): ?User
+    {
+        return $this->user;
+    }
+
+    public function setUser(?User $user): static
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+
+    public function getSettingKey(): ?string
+    {
+        return $this->settingKey;
+    }
+
+    public function setSettingKey(string $settingKey): static
+    {
+        $this->settingKey = $settingKey;
+
+        return $this;
+    }
+
+    public function getValue(): ?string
+    {
+        return $this->value;
+    }
+
+    public function setValue(string $value): static
+    {
+        $this->value = $value;
+
+        return $this;
+    }
+}
