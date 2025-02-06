f115f28ac4691de2b53f19eaf744d55ce7347fcb
antonin.p <antonin.p@lahautesociete.com>
2025-02-06 10:22:10 +0100

    feat : basic routes for exercises management

---
+++ migrations/Version20250205143418.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20250205143418 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE exercise ADD created_by_id INT DEFAULT NULL, ADD status SMALLINT DEFAULT NULL');
+        $this->addSql('ALTER TABLE exercise ADD CONSTRAINT FK_AEDAD51CB03A8386 FOREIGN KEY (created_by_id) REFERENCES user (id)');
+        $this->addSql('CREATE INDEX IDX_AEDAD51CB03A8386 ON exercise (created_by_id)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE exercise DROP FOREIGN KEY FK_AEDAD51CB03A8386');
+        $this->addSql('DROP INDEX IDX_AEDAD51CB03A8386 ON exercise');
+        $this->addSql('ALTER TABLE exercise DROP created_by_id, DROP status');
+    }
+}
---
+++ src/State/ExerciseDataPersister.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\State;
+
+use ApiPlatform\Metadata\Operation;
+use ApiPlatform\State\ProcessorInterface;
+use App\Entity\Exercise;
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\SecurityBundle\Security;
+use Symfony\Component\HttpKernel\Exception\HttpException;
+
+class ExerciseDataPersister implements ProcessorInterface
+{
+    private EntityManagerInterface $entityManager;
+    private Security $security;
+
+    public function __construct(EntityManagerInterface $entityManager, Security $security)
+    {
+        $this->entityManager = $entityManager;
+        $this->security = $security;
+    }
+
+
+    public function process(mixed $data, Operation $operation, array $uriVariables = [], array $context = []): Exercise
+    {
+        if (!$data instanceof Exercise) {
+            throw new HttpException(400, "Données invalides.");
+        }
+
+        $user = $this->entityManager->getRepository(User::class)->findOneBy(['email' => $this->security->getUser()->getUserIdentifier()]);
+        if (!$user) {
+            throw new HttpException(403, "Utilisateur non trouvé. $this->security->getUser()->getUserIdentifier()");
+        }
+
+        $data->setCreatedBy($user);
+
+        $data->setStatus(0);
+
+        $this->entityManager->persist($data);
+        $this->entityManager->flush();
+
+        return $data;
+    }
+}
