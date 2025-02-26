da4316f96345bde84c910e1241fa7c3ec7a07ea2
antonin.p <antonin.p@lahautesociete.com>
2025-02-26 17:07:24 +0100

    feat : plan api requests CRUD

--- src/Entity/Plan.php
+++ src/Entity/Plan.php
@@ -3,32 +3,93 @@
 namespace App\Entity;
 
 use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\Delete;
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\GetCollection;
+use ApiPlatform\Metadata\Patch;
+use ApiPlatform\Metadata\Post;
+use App\Controller\Plan\GetUserPlansController;
 use App\Repository\PlanRepository;
+use App\State\PlanDataPersister;
+use App\State\PlanUpdatePersister;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
+use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
+use Symfony\Component\Validator\Constraints\Choice;
 
 #[ORM\Entity(repositoryClass: PlanRepository::class)]
 #[ApiResource(
+    operations: [
+        new GetCollection(
+            security: "is_granted('ROLE_ADMIN')",
+            name: 'get_all_plans'
+        ),
+        new GetCollection(
+            uriTemplate: '/plans/me',
+            controller: GetUserPlansController::class,
+            normalizationContext: ['groups' => ['plan:read']],
+            security: "is_granted('ROLE_USER')",
+            read: false,
+            name: 'get_user_plans'
+        ),
+        new Post(
+            security: "is_granted('ROLE_USER')",
+            validationContext: ['groups' => ['Default', 'plan:create']],
+            processor: PlanDataPersister::class,
+        ),
+        new Patch(
+            security: "object.getUser() == user",
+            securityMessage: "Tu ne peux modifier que les plans que tu as créés.",
+            validationContext: ['groups' => ['Default', 'plan:update']],
+            processor: PlanUpdatePersister::class,
+        ),
+        new Delete(
+            security: "object.getUser() == user",
+            securityMessage: "Tu ne peux supprimer que les plans que tu as créés.",
+            name: 'delete_plan'
+        ),
+        new Get(
+            security: "object.getUser() == user",
+            securityMessage: "Tu ne peux voir que les plans que tu as créés.",
+            name: 'get_plan'
+        )
+    ],
+    normalizationContext: ['groups' => ['plan:read']],
+    denormalizationContext: ['groups' => ['plan:write']],
     security: "is_granted('ROLE_USER')"
 )]
 class Plan
 {
+    public const DAYS = [
+        'MONDAY' => 'monday',
+        'TUESDAY' => 'tuesday',
+        'WEDNESDAY' => 'wednesday',
+        'THURSDAY' => 'thursday',
+        'FRIDAY' => 'friday',
+        'SATURDAY' => 'saturday',
+        'SUNDAY' => 'sunday',
+    ];
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
     private ?int $id = null;
 
     #[ORM\Column(length: 255, nullable: true)]
+    #[Groups(['plan:read', 'plan:write'])]
     private ?string $name = null;
 
     #[ORM\Column(length: 255, nullable: true)]
+    #[Groups(['plan:read', 'plan:write'])]
     private ?string $goal = null;
 
     #[ORM\Column]
+    #[Groups(['plan:read'])]
     private ?\DateTimeImmutable $createdAt = null;
 
     #[ORM\ManyToOne(inversedBy: 'plans')]
+    #[Groups(['plan:read'])]
     private ?User $user = null;
 
     /**
@@ -36,6 +97,25 @@ class Plan
      */
     #[ORM\OneToMany(targetEntity: Session::class, mappedBy: 'plan')]
     private Collection $sessions;
+
+    #[ORM\Column(type: Types::JSON, nullable: true)]
+    #[Groups(['plan:read', 'plan:write'])]
+    #[Choice(
+        choices: [
+            'monday',
+            'tuesday',
+            'wednesday',
+            'thursday',
+            'friday',
+            'saturday',
+            'sunday'
+        ],
+        multiple: true
+    )]
+    private ?array $trainingDays = [];
+
+    #[ORM\Column(nullable: true)]
+    private ?\DateTimeImmutable $updatedAt = null;
 
     public function __construct()
     {
@@ -121,6 +201,53 @@ class Plan
                 $session->setPlan(null);
             }
         }
+
+        return $this;
+    }
+
+    public function getTrainingDays(): array
+    {
+        return $this->trainingDays ?? [];
+    }
+
+    public function setTrainingDays(array $days): static
+    {
+        $validDays = array_values(self::DAYS);
+
+        foreach ($days as $day) {
+            if (!in_array($day, $validDays, true)) {
+                throw new \InvalidArgumentException("Jour invalide : $day");
+            }
+        }
+
+        $this->trainingDays = array_unique($days);
+        return $this;
+    }
+
+    #[Groups(['plan:read'])]
+    public function getFormattedTrainingDays(): array
+    {
+        $map = [
+            'monday' => 'Lundi',
+            'tuesday' => 'Mardi',
+            'wednesday' => 'Mercredi',
+            'thursday' => 'Jeudi',
+            'friday' => 'Vendredi',
+            'saturday' => 'Samedi',
+            'sunday' => 'Dimanche'
+        ];
+
+        return array_map(fn($day) => $map[$day] ?? $day, $this->trainingDays);
+    }
+
+    public function getUpdatedAt(): ?\DateTimeImmutable
+    {
+        return $this->updatedAt;
+    }
+
+    public function setUpdatedAt(?\DateTimeImmutable $updatedAt): static
+    {
+        $this->updatedAt = $updatedAt;
 
         return $this;
     }
